622b20061dbc199142240fdc1a5071562b147f44
moetez1233 <moetezmaddouri@gmail.com>
2023-10-23 11:23:30 +0100

    add acteur modificatin

--- src/main/java/com/app/FirstApp/services/Acteur/ActeruServImpl.java
+++ src/main/java/com/app/FirstApp/services/Acteur/ActeruServImpl.java
@@ -7,6 +7,7 @@ import com.app.FirstApp.repository.acteur.ActeurRepo;
 import com.app.FirstApp.repository.user.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
